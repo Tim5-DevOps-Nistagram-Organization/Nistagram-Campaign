eb7f69509a6df5dfbe0d3c3d0ed2554f3bfff25f
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-06 18:14:50 +0200

    init project #8 (#10)

---
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramcampaign/NistagramCampaignApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.devops.tim5.nistagramcampaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NistagramCampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NistagramCampaignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/uns/ftn/devops/tim5/nistagramcampaign/NistagramCampaignApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.devops.tim5.nistagramcampaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NistagramCampaignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
