2c1736bfead0ff58bd3fb7b1892ee113b8424a13
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-06 17:32:47 +0200

    Update create-test-env-run-test.sh

--- create-test-env-run-test.sh
+++ create-test-env-run-test.sh
@@ -31,7 +31,3 @@ while ! is_finished nistagram-campaign-microservice; do sleep 20; done
 # provera Quality Gate-a i da li je neki od testova pao
 servers_logs=$(docker logs nistagram-campaign --tail 20)
 python ./sonar-maven-breaker.py --testLogs "${servers_logs}" --projectKey ${SONAR_PROJ_KEY_SVC}  --pullRequestNumber ${pullKey} --branch ${branch}
-
-
-
-
