5afd006642f70411dd774d4c83eebce2e04a3c9d
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-25 12:35:57 +0200

    Set up auth #14

---
+++ src/main/java/rs/ac/uns/ftn/devops/tim5/nistagramcampaign/kafka/Constants.java
@@ -0,0 +1,8 @@
+package rs.ac.uns.ftn.devops.tim5.nistagramcampaign.kafka;
+
+public class Constants {
+
+    public static final String GROUP = "my_group_id";
+    public static final String AUTH_TOPIC = "auth_topic";
+    public static final String AUTH_CAMPAIGN_TOPIC = "auth_campaign_topic";
+}
