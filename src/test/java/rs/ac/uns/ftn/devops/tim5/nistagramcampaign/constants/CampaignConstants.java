86c95d54dda0c34abd9e6cd241ff9e8e31ae3d3a
DusanBucan <34024330+DusanBucan@users.noreply.github.com>
2021-07-07 09:17:33 +0200

    tests for campaign service (#20)

---
+++ src/test/java/rs/ac/uns/ftn/devops/tim5/nistagramcampaign/constants/CampaignConstants.java
@@ -0,0 +1,14 @@
+package rs.ac.uns.ftn.devops.tim5.nistagramcampaign.constants;
+
+
+
+public class CampaignConstants {
+
+    private CampaignConstants() {}
+
+    public static int NUM_SHOWS_PER_DAY_MULTIPLE = 3;
+    public static int NUM_SHOWS_PER_DAY_SINGLE = 1;
+
+    public static String GET_ALL_ACTIVE_BY_AGENT = "/campaign/active/";
+
+}
